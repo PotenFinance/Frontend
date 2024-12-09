371f272bc6de30201fd4abb53b72e675d09745e9
danimkim <danimkim.dev@gmail.com>
2024-12-10 02:46:41 +0900

    chore: import alias 설정 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { Global } from '@emotion/react';
+import Layout from '@components/layout';
 import type { AppProps } from 'next/app';
-import { global } from '@src/styles/globals';
-import Layout from '@src/components/layout';
+import { global } from '@styles/globals';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
