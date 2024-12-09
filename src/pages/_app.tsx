eb1862858321395428f098aba4c5e3feb97a04bc
danimkim <danimkim.dev@gmail.com>
2024-12-10 01:51:16 +0900

    WIP: 공통 레이아웃 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
-import '@/styles/globals.css';
 import { Global } from '@emotion/react';
 import type { AppProps } from 'next/app';
 import { global } from '@src/styles/globals';
+import Layout from '@src/components/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Layout>
       <Global styles={global} />
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
