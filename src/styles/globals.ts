d46b23953f9cf12ba4435ae1ec99e190ad51c903
danimkim <danimkim.dev@gmail.com>
2024-12-10 01:01:53 +0900

    chore: 웹폰트 추가

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -7,7 +7,9 @@ export const global = css`
   }
 
   body {
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: 'Pretendard JP', 'Paperlogy', 'S-CoreDream', Arial, Helvetica, sans-serif,
+      -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans,
+      Droid Sans;
   }
 
   * {
@@ -24,5 +26,34 @@ export const global = css`
   // css reset
   ul {
     list-style: none;
+  }
+
+  // Web fonts
+  @font-face {
+    // 페이퍼로지 600
+    font-family: 'Paperlogy';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2408-3@1.0/Paperlogy-6SemiBold.woff2')
+      format('woff2');
+    font-weight: 600;
+    font-style: normal;
+  }
+
+  // 페이퍼로지 700
+  @font-face {
+    // 페이퍼 로지 600
+    font-family: 'Paperlogy';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2408-3@1.0/Paperlogy-7Bold.woff2')
+      format('woff2');
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  // 에스코어드림
+  @font-face {
+    font-family: 'S-CoreDream';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-6Bold.woff')
+      format('woff');
+    font-weight: 600;
+    font-style: normal;
   }
 `;
