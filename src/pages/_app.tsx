2437ee39c7349f9817597b7f52552c604c5977ff
yujinyny <hyjinny00@gmail.com>
2024-12-12 22:04:10 +0900

    feat: QueryClientProvider 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,19 @@ import Layout from '@components/layout';
 import type { AppProps } from 'next/app';
 import { global } from '@styles/globals';
 import { lightTheme } from '@styles/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <Global styles={global} />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={lightTheme}>
+        <Global styles={global} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
