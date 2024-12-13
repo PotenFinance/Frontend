6f5ffa2f419e66ce0ac4c5ea2ff34b8c654ad067
danimkim <danimkim.dev@gmail.com>
2024-12-14 00:58:39 +0900

    refactor: 하단 nav tab 활성/비활성 상태 style 수정

--- src/components/layout/NavTab.tsx
+++ src/components/layout/NavTab.tsx
@@ -1,31 +1,45 @@
 import { NAV_MENU } from '@constants/index';
+import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
 import Link from 'next/link';
-import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 export default function NavTab() {
-  const [activeTabPathName, setActiveTabPathName] = useState('/');
-
-  const handleTabClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    setActiveTabPathName(e.currentTarget.pathname);
-  };
+  const pathname = usePathname();
+  const theme = useTheme();
 
   return (
     <Nav>
       {NAV_MENU.map(menu => {
         const Icon = menu.icon;
 
+        const isActive =
+          menu.path === '/' ? pathname === menu.path : pathname.startsWith(menu.path);
+
         return (
-          <Tab
-            key={menu.name}
-            href={menu.path}
-            menuname={menu.name}
-            onClick={handleTabClick}
-            active={activeTabPathName === menu.path}
-          >
-            <Icon className={`tab-icon ${menu.path.slice(1)}`} width={25} height={25} />
-            <TabName active={activeTabPathName === menu.path}>{menu.name}</TabName>
-          </Tab>
+          <Link key={menu.name} href={menu.path} style={{ width: '100%', height: '100%' }}>
+            <Tab menuname={menu.name} active={isActive}>
+              <Icon
+                width={25}
+                height={25}
+                fill={
+                  menu.path === '/'
+                    ? 'none'
+                    : isActive
+                    ? theme.color.base.white
+                    : theme.color.base.black
+                }
+                stroke={
+                  menu.path === '/' && isActive
+                    ? theme.color.base.white
+                    : menu.path === '/' && !isActive
+                    ? theme.color.base.black
+                    : undefined
+                }
+              />
+              <TabName active={isActive}>{menu.name}</TabName>
+            </Tab>
+          </Link>
         );
       })}
     </Nav>
@@ -45,7 +59,7 @@ const Nav = styled.nav`
   box-shadow: 0px -4px 4px 0px rgba(0, 0, 0, 0.1);
 `;
 
-const Tab = styled(Link)<{ active: boolean; menuname: string }>`
+const Tab = styled.div<{ active: boolean; menuname: string }>`
   width: 100%;
   height: 100%;
   background-color: ${({ theme, active }) => (active ? theme.color.primary._1 : 'transparent')};
@@ -55,36 +69,6 @@ const Tab = styled(Link)<{ active: boolean; menuname: string }>`
   align-items: center;
   border-top-right-radius: ${({ menuname }) => (menuname !== '마이페이지' ? '16px' : 0)};
   border-top-left-radius: ${({ menuname }) => (menuname !== '홈' ? '16px' : 0)};
-
-  // 활성화 탭
-  &.active {
-    // 활성화 탭 아이콘
-    & .tab-icon {
-      // 홈
-      & g {
-        stroke: ${({ theme }) => theme.color.base.white};
-      }
-
-      // 소비리포트
-      &.report g path {
-        fill: ${({ theme }) => theme.color.base.white};
-      }
-
-      // 커뮤니티
-      &.community {
-        path {
-          fill: ${({ theme }) => theme.color.base.white};
-        }
-      }
-
-      // 마이페이지
-      &.mypage {
-        g path {
-          fill: ${({ theme }) => theme.color.base.white};
-        }
-      }
-    }
-  }
 `;
 
 const TabName = styled.span<{ active: boolean }>`
