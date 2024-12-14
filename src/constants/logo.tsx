6002404613d6b3d53d454b3a2d121c5836ad7c53
yujinyny <hyjinny00@gmail.com>
2024-12-14 17:00:35 +0900

    refactor: icons/logo index 파일 생성

--- src/constants/logo.tsx
+++ src/constants/logo.tsx
@@ -1,34 +1,39 @@
-import AppleMusicLargeLogo from '@assets/icons/logo/large/AppleMusic';
-import BaeminLargeLogo from '@assets/icons/logo/large/Baemin';
-import CoupangLargeLogo from '@assets/icons/logo/large/Coupang';
-import DisneyLargeLogo from '@assets/icons/logo/large/Disney';
-import KakaoLargeLogo from '@assets/icons/logo/large/Kakao';
-import KurlyLargeLogo from '@assets/icons/logo/large/Kurly';
-import MelonLargeLogo from '@assets/icons/logo/large/Melon';
-import NaverLargeLogo from '@assets/icons/logo/large/Naver';
-import NetflixLargeLogo from '@assets/icons/logo/large/Netflix';
-import SpotifyLargeLogo from '@assets/icons/logo/large/Spotify';
-import TvingLargeLogo from '@assets/icons/logo/large/Tving';
-import WatchaLargeLogo from '@assets/icons/logo/large/Watcha';
-import WavveLargeLogo from '@assets/icons/logo/large/Wavve';
-import YogiyoLargeLogo from '@assets/icons/logo/large/Yogiyo';
-import YoutubeLargeLogo from '@assets/icons/logo/large/Youtube';
-
-import AppleMusicSmallLogo from '@assets/icons/logo/small/AppleMusic';
-import BaeminSmallLogo from '@assets/icons/logo/small/Baemin';
-import CoupangSmallLogo from '@assets/icons/logo/small/Coupang';
-import DisneySmallLogo from '@assets/icons/logo/small/Disney';
-import KakaoSmallLogo from '@assets/icons/logo/small/Kakao';
-import KurlySmallLogo from '@assets/icons/logo/small/Kurly';
-import MelonSmallLogo from '@assets/icons/logo/small/Melon';
-import NaverSmallLogo from '@assets/icons/logo/small/Naver';
-import NetflixSmallLogo from '@assets/icons/logo/small/Netflix';
-import SpotifySmallLogo from '@assets/icons/logo/small/Spotify';
-import TvingSmallLogo from '@assets/icons/logo/small/Tving';
-import WatchaSmallLogo from '@assets/icons/logo/small/Watcha';
-import WavveSmallLogo from '@assets/icons/logo/small/Wavve';
-import YogiyoSmallLogo from '@assets/icons/logo/small/Yogiyo';
-import YoutubeSmallLogo from '@assets/icons/logo/small/Youtube';
+import {
+  AppleMusicLargeLogo,
+  BaeminLargeLogo,
+  CoupangLargeLogo,
+  DisneyLargeLogo,
+  KakaoLargeLogo,
+  KurlyLargeLogo,
+  MelonLargeLogo,
+  NaverLargeLogo,
+  NetflixLargeLogo,
+  SpotifyLargeLogo,
+  TvingLargeLogo,
+  WatchaLargeLogo,
+  WavveLargeLogo,
+  YogiyoLargeLogo,
+  YoutubeLargeLogo,
+  YoutubeMusicLargeLogo,
+} from '@assets/icons/logo/large';
+import {
+  AppleMusicSmallLogo,
+  BaeminSmallLogo,
+  CoupangSmallLogo,
+  DisneySmallLogo,
+  KakaoSmallLogo,
+  KurlySmallLogo,
+  MelonSmallLogo,
+  NaverSmallLogo,
+  NetflixSmallLogo,
+  SpotifySmallLogo,
+  TvingSmallLogo,
+  WatchaSmallLogo,
+  WavveSmallLogo,
+  YogiyoSmallLogo,
+  YoutubeSmallLogo,
+  YoutubeMusicSmallLogo,
+} from '@assets/icons/logo/small';
 
 interface IProps {
   width?: string | number;
@@ -41,7 +46,7 @@ export const BRAND_LOGO = (props: IProps) => ({
     E001: <KakaoLargeLogo {...props} />,
     M001: <MelonLargeLogo {...props} />,
     M002: <AppleMusicLargeLogo {...props} />,
-    M003: <YoutubeLargeLogo {...props} />,
+    M003: <YoutubeMusicLargeLogo {...props} />,
     M004: <SpotifyLargeLogo {...props} />,
     O001: <NetflixLargeLogo {...props} />,
     O002: <DisneyLargeLogo {...props} />,
@@ -59,7 +64,7 @@ export const BRAND_LOGO = (props: IProps) => ({
     E001: <KakaoSmallLogo {...props} />,
     M001: <MelonSmallLogo {...props} />,
     M002: <AppleMusicSmallLogo {...props} />,
-    M003: <YoutubeSmallLogo {...props} />,
+    M003: <YoutubeMusicSmallLogo {...props} />,
     M004: <SpotifySmallLogo {...props} />,
     O001: <NetflixSmallLogo {...props} />,
     O002: <DisneySmallLogo {...props} />,
